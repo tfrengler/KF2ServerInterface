aea71a3200126578c1842996fbaf089e0f2ffbb5
Thomas Frengler <thomasfrengler@gmail.com>
2019-06-24 07:23:07 +0200

    Initial commit

---
+++ KF2ServerInterface/KF2ServerHandler.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace KF2ServerInterface
+{
+    public class KF2ServerHandler
+    {
+        #region PROPERTIES
+        private readonly CookieContainer cookies;
+        private readonly HttpClientHandler handler;
+        private readonly HttpClient httpClient;
+
+        public struct Server
+        {
+            public string name;
+            public Uri address;
+            public string gamemode;
+            public string configDir;
+            public string desiredMap;
+        }
+
+        public string[] maps { get; } = new string[]
+        {
+            "KF-Airship",
+            "KF-BioticsLab",
+            "KF-BlackForest",
+            "KF-BurningParis",
+            "KF-Catacombs",
+            "KF-ContainmentStation",
+            "KF-DieSector",
+            "KF-EvacuationPoint",
+            "KF-Farmhouse",
+            "KF-HostileGrounds",
+            "KF-InfernalRealm",
+            "KF-KrampusLair",
+            "KF-Lockdown",
+            "KF-MonsterBall",
+            "KF-Nightmare",
+            "KF-Nuked",
+            "KF-Outpost",
+            "KF-PowerCore_Holdout",
+            "KF-Prison",
+            "KF-SantasWorkshop",
+            "KF-ShoppingSpree",
+            "KF-Spillway",
+            "KF-SteamFortress",
+            "KF-TheDescent",
+            "KF-TragicKingdom",
+            "KF-VolterManor",
+            "KF-ZedLanding"
+        };
+        #endregion
+
+        //CONSTRUCTOR
+        public KF2ServerHandler()
+        {
+            this.cookies = new CookieContainer();
+            this.handler = new HttpClientHandler();
+            this.handler.AllowAutoRedirect = false;
+            this.handler.CookieContainer = cookies;
+            this.httpClient = new HttpClient(handler);
+        }
+
+        public async Task<string> GetLoginToken(Uri serverAddress)
+        {
+            HttpResponseMessage loginPageResponse = await this.SendGetRequest(serverAddress);
+            if (loginPageResponse.StatusCode != HttpStatusCode.OK) return "";
+
+            string sessionID = this.GetHeaderValue(loginPageResponse.Headers, "Set-Cookie", "sessionid");
+            if (sessionID.Length == 0) return "";
+
+            string responseBody = await loginPageResponse.Content.ReadAsStringAsync();
+
+            Match tokenSearch = new Regex("name=\"token\" value=\"(.*)\"").Match(responseBody);
+            if (!tokenSearch.Success) return "";
+
+            this.cookies.Add(serverAddress, new Cookie("sessionid", sessionID));
+            return tokenSearch.Groups[1].Value;
+        }
+
+        public async Task<string> Login(Uri serverAddress, string token, string username, string password)
+        {
+            Dictionary<string, string> postData = new Dictionary<string, string>
+            {
+                { "token", token },
+                { "password_hash", "" },
+                { "username", username },
+                { "password", password },
+                { "remember", "-1" }
+            };
+
+            HttpResponseMessage loginResponse = await this.SendPostRequest(serverAddress, postData);
+            if (loginResponse.StatusCode != HttpStatusCode.OK) return "";
+            string authToken = this.GetHeaderValue(loginResponse.Headers, "Set-Cookie", "authcred");
+
+            if (authToken.Length == 0)
+                return "";
+
+            this.cookies.Add(serverAddress, new Cookie("authcred", authToken));
+            return authToken;
+        }
+
+        private async void DumpResponseHeadersAndBody(HttpResponseMessage response)
+        {
+            foreach (KeyValuePair<string, IEnumerable<string>> header in response.Content.Headers)
+            {
+                Console.WriteLine($"{header.Key}: {String.Join(" ", header.Value)}");
+            }
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+        }
+
+        private void ClearSessionData(Uri address)
+        {
+            foreach (Cookie cookie in this.cookies.GetCookies(address))
+                cookie.Expired = true;
+        }
+
+        public async Task<int> GetPlayerCount(Uri serverAddress)
+        {
+            HttpResponseMessage infoPageResponse = await this.SendGetRequest(new Uri(serverAddress + "current/info"));
+            string responseBody = await infoPageResponse.Content.ReadAsStringAsync();
+
+            Match playerCountSearch = new Regex("<dl id=\"currentRules\">[\\s\\S]+?<dd>(\\d)\\/6</dd>").Match(responseBody);
+
+            if (!playerCountSearch.Success)
+                return -1;
+
+            int playerCount = Int16.Parse(playerCountSearch.Groups[1].Value);
+            Console.WriteLine("Player count: " + playerCount);
+            return playerCount;
+        }
+
+        public async Task<string> GetCurrentMap(Uri serverAddress)
+        {
+            HttpResponseMessage httpResponse = await this.SendGetRequest(new Uri(serverAddress + "current/change"));
+            if (httpResponse.StatusCode != HttpStatusCode.OK)
+                return "";
+
+            string responseBody = await httpResponse.Content.ReadAsStringAsync();
+            Match currentMapSearch = new Regex("<select id=\"map\" name=\"map\">[\\s\\S]+?<option value=\"(.*)\" selected=\"selected\">").Match(responseBody);
+
+            if (!currentMapSearch.Success)
+                return "";
+
+            return currentMapSearch.Groups[1].Value;
+        }
+
+        public async Task<bool> SwitchMap(string mapName, Server serverInstance)
+        {
+            if (!this.maps.Contains(mapName)) return false;
+
+            Dictionary<string, string> postData = new Dictionary<string, string>
+            {
+                { "gametype", serverInstance.gamemode },
+                { "map", mapName },
+                { "mutatorGroupCount", "0" },
+                { "urlextra", $"?ConfigSubDir={serverInstance.configDir}" },
+                { "action", "change" }
+            };
+
+            HttpResponseMessage httpResponse = await this.SendPostRequest(new Uri(serverInstance.address + "current/change"), postData);
+            if (httpResponse.StatusCode != HttpStatusCode.OK) return false;
+
+            string newSessionID = this.GetHeaderValue(httpResponse.Headers, "Set-Cookie", "sessionid");
+
+            if (newSessionID.Length > 0)
+            {
+                this.ClearSessionData(serverInstance.address);
+                this.cookies.Add(serverInstance.address, new Cookie("sessionid", newSessionID));
+            }
+
+            string responseBody = await httpResponse.Content.ReadAsStringAsync();
+            Match changedMapSearch = new Regex("Changing the game. This could take a little while...").Match(responseBody);
+
+            if (changedMapSearch.Success)
+                return true;
+
+            return false;
+        }
+
+        private async Task<HttpResponseMessage> SendGetRequest(Uri address)
+        {
+            HttpResponseMessage httpResponse;
+
+            try { 
+                httpResponse = await httpClient.GetAsync(address);
+                return httpResponse;
+            }
+            catch(HttpRequestException error) {
+                Console.WriteLine($"ERROR: Sending GET request to {address} failed");
+                Console.WriteLine(error.Message);
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            };
+        }
+
+        private async Task<HttpResponseMessage> SendPostRequest(Uri address, Dictionary<string, string> postData)
+        {
+            HttpResponseMessage httpResponse;
+            HttpContent formData = new FormUrlEncodedContent(postData);
+            try
+            {
+                httpResponse = await httpClient.PostAsync(address, formData);
+                return httpResponse;
+            }
+            catch(HttpRequestException error)
+            {
+                Console.WriteLine($"ERROR: Sending POST request to {address} failed");
+                Console.WriteLine(error.Message);
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            };
+        }
+
+        private string GetHeaderValue(HttpResponseHeaders headers, string headerName, string headerKey)
+        {
+            if (!headers.TryGetValues(headerName, out var headerOut))
+                return "";
+
+            string headerValues = String.Join(",", headerOut.ToArray());
+            Match valueSearch = new Regex($"{headerKey}=\"(.*)\"").Match(headerValues);
+
+            if (valueSearch.Success)
+                return valueSearch.Groups[1].Value;
+
+            return "";
+        }
+    }
+}
---
+++ KF2ServerInterface/Program.cs
@@ -0,0 +1,132 @@
+﻿#define DEBUG
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KF2ServerInterface
+{
+    class Program
+    {
+        const string GLOBAL_DESIRED_MAP = "KF-SteamFortress";
+        const string SERVER_ADDRESS = "http://192.168.1.222";
+        const int CHECK_INTERVAL = 300000;
+
+        static void Main(string[] args)
+        {
+            KF2ServerHandler KF2Server = new KF2ServerHandler();
+            KF2ServerHandler.Server[] servers = new KF2ServerHandler.Server[5];
+
+            #region SERVER INSTANCES
+            servers.SetValue(new KF2ServerHandler.Server
+            {
+                name = "PRIVATE",
+                gamemode = "KFGameContent.KFGameInfo_Endless",
+                address = new Uri(Program.SERVER_ADDRESS + ":8000/ServerAdmin/"),
+                configDir = "Private",
+                desiredMap = Program.GLOBAL_DESIRED_MAP
+            }, 0);
+
+            servers.SetValue(new KF2ServerHandler.Server
+            {
+                name = "HARD",
+                gamemode = "KFGameContent.KFGameInfo_Survival",
+                address = new Uri(Program.SERVER_ADDRESS + ":8001/ServerAdmin/"),
+                configDir = "Hard",
+                desiredMap = Program.GLOBAL_DESIRED_MAP
+            }, 1);
+
+            servers.SetValue(new KF2ServerHandler.Server
+            {
+                name = "SUICIDAL",
+                gamemode = "KFGameContent.KFGameInfo_Survival",
+                address = new Uri(Program.SERVER_ADDRESS + ":8002/ServerAdmin/"),
+                configDir = "Suicidal",
+                desiredMap = Program.GLOBAL_DESIRED_MAP
+            }, 2);
+
+            servers.SetValue(new KF2ServerHandler.Server
+            {
+                name = "HELL ON EARTH",
+                gamemode = "KFGameContent.KFGameInfo_Survival",
+                address = new Uri(Program.SERVER_ADDRESS + ":8003/ServerAdmin/"),
+                configDir = "HoE",
+                desiredMap = Program.GLOBAL_DESIRED_MAP
+            }, 3);
+
+            servers.SetValue(new KF2ServerHandler.Server
+            {
+                name = "WEEKLY",
+                gamemode = "KFGameContent.KFGameInfo_Weekly",
+                address = new Uri(Program.SERVER_ADDRESS + ":8004/ServerAdmin/"),
+                configDir = "Weekly",
+                desiredMap = Program.GLOBAL_DESIRED_MAP
+            }, 4);
+            #endregion
+
+            while (true)
+            {
+                foreach (KF2ServerHandler.Server currentServer in servers)
+                {
+                    Console.WriteLine($"---------------------CHECKING SERVER: {currentServer.name}");
+
+                    Console.WriteLine("Getting login token");
+                    Task<string> tokenTask = KF2Server.GetLoginToken(currentServer.address);
+                    while (!tokenTask.IsCompleted) ;
+
+                    string loginToken = tokenTask.Result;
+
+                    if (loginToken.Length == 0)
+                    {
+                        Console.WriteLine("ERROR: No login token acquired" + Environment.NewLine);
+                        continue;
+                    }
+
+                    Console.WriteLine($"SUCCESS: Login token acquired ({loginToken})");
+
+                    Console.WriteLine("Logging in");
+                    Task<string> loginTask = KF2Server.Login(currentServer.address, loginToken, "admin", "ThomaS_86954494?");
+                    while (!loginTask.IsCompleted) ;
+
+                    string authToken = loginTask.Result;
+
+                    if (authToken.Length == 0)
+                    {
+                        Console.WriteLine("ERROR: No auth token acquired" + Environment.NewLine);
+                        continue;
+                    }
+
+                    Console.WriteLine($"SUCCESS: Logged in ({authToken})");
+
+                    Task<int> getPlayerCountTask = KF2Server.GetPlayerCount(currentServer.address);
+                    while (!getPlayerCountTask.IsCompleted) ;
+                    int playerCount = getPlayerCountTask.Result;
+
+                    Task<string> getCurrentMapTask = KF2Server.GetCurrentMap(currentServer.address);
+                    while (!getCurrentMapTask.IsCompleted) ;
+                    string currentMap = getCurrentMapTask.Result;
+
+                    if (playerCount == 0 && currentMap != currentServer.desiredMap)
+                    {
+                        Console.WriteLine($"Server is empty and not on the right map ({currentMap}). Switching to: {currentServer.desiredMap}" + Environment.NewLine);
+                        /*
+                        Task<bool> mapSwitchTask = KF2Server.SwitchMap(currentServer.desiredMap, currentServer);
+                        while (!mapSwitchTask.IsCompleted) ;
+
+                        if (mapSwitchTask.Result)
+                            Console.WriteLine("Map switch successful");
+                        */
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Server has {playerCount} players on map {currentMap}, doing nothing" + Environment.NewLine);
+                    }
+                }
+
+                Console.WriteLine($"Waiting... ({Program.CHECK_INTERVAL / 1000} seconds)");
+                Console.WriteLine("-------------------------------------------------------------" + Environment.NewLine);
+                Thread.Sleep(Program.CHECK_INTERVAL);
+            }
+        }
+    }
+}
